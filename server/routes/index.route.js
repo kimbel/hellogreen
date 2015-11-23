18dfcfc723965b50c3202a3865cc97e994c84d68
Axel Gendillard <agendillard@gmail.com>
2015-11-23 11:08:13 +0100

    fix svg logo into index.html

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -3,7 +3,18 @@ module.exports = function(app) {
 
 	/* serves main page */
 	app.get("/", function(req, res) {
-    	res.sendfile('./client/index.html')
+    	res.sendfile('./client/index.html');
 	});
 
+	app.get("/style.css", function(req, res) {
+    	res.sendfile('./client/style.css');
+	});
+
+	app.get("/img/hellogreen-logo-no-background.svg", function(req, res) {
+    	res.sendfile('./client/img/hellogreen-logo-no-background.svg');
+	});
+
+	app.get("/favicon.ico", function(req, res) {
+    	res.sendfile('./client/img/favicon.ico');
+	});
 };
